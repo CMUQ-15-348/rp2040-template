2de81b6603590374c126a2a59c3fb566720e9b33
Ryan Riley <rriley.cs@gmail.com>
2025-09-11 10:32:02 +0300

    Better clock initialization code

--- src/lib348/sys_clock.rs
+++ src/lib348/sys_clock.rs
@@ -1,5 +1,8 @@
 use crate::lib348::control_registers::*;
 
+pub const RESET_PLL_SYS: u32 = 12; // Select PLL_SYS to be reset
+pub const RESET_PLL_USB: u32 = 13; // Select PLL_USB to be reset
+
 /*
  * Configure the system clock to 125 MHz.
  * There is a nice reference in the SDK:
@@ -19,66 +22,80 @@ pub fn init_clocks() {
         // Wait for the XOSC to be ready
     }
 
-    // Set the CLK_REF glitchless mux to 2 (Ring oscillator)
-    write_reg(CLOCKS_BASE + 0x30, 2);
-    while read_reg(CLOCKS_BASE + 0x30) != 2 {
-        // Wait for the glitchless mux to be set to 2
-    }
     // Set the CLK_SYS glitchless mux to 0 (CLK_REF) so that we can mess with the CLK_SYS sources without causing issues.
-    clear_bits(CLOCKS_BASE + 0x3c, 0x0000_0001);
-    while read_reg(CLOCKS_BASE + 0x3c) & 0x0000_0001 != 0 {
-        // Wait for the glitchless mux to be set to 0
+    clear_bits(CLOCKS_BASE + 0x3c, 0x1);
+    while read_reg(CLOCKS_BASE + 0x44) != 0x1 {
+        // Wait for the glitchless mux to be set
     }
 
-    // Reset, then deassert the reset on PLL_SYS
-    // See Section 2.14 in the datasheet for details
-    set_bits(RESETS_BASE, 1 << 12); // Write 1 to reset
-    clear_bits(RESETS_BASE, 1 << 12); // Write 0 to deassert reset
-
-    // Reset, then deassert the reset on PLL_USB
-    // See Section 2.14 in the datasheet for details
-    set_bits(RESETS_BASE, 1 << 13); // Write 1 to reset
-    clear_bits(RESETS_BASE, 1 << 13); // Write 0 to deassert reset
-
-    // Disable the PLL while we reconfigure it
-    write_reg(PLL_SYS_BASE + 0x04, 1 << 5 | 1); // Set the PD and VCO bits to 1 to disable the PLL
-
-    // Configure the PLL System divider (See 2.18.2.1)
-    write_reg(PLL_SYS_BASE + 0x08, 125); // fbdiv = 125
-    write_reg(PLL_SYS_BASE + 0x0C, 0x0006_2000); // PD1=6, PD2=2
-    clear_bits(PLL_SYS_BASE + 0x04, 0x0000_0011); // Clear the PD and VCO bits to enable the PLL
-    while read_reg(PLL_SYS_BASE + 0x00) & 0x8000_0000 == 0 {
-        // Wait for the PLL to be ready
+    // Set the CLK_REF glitchless mux to 0 (Ring oscillator)
+    clear_bits(CLOCKS_BASE + 0x30, 0b11);
+    while read_reg(CLOCKS_BASE + 0x38) != 1 {
+        // Wait for the glitchless mux to be set
     }
 
-    // Configure the PLL USB divider
-    write_reg(PLL_USB_BASE + 0x08, 100); // fbdiv = 100
-    write_reg(PLL_USB_BASE + 0x0C, 0x0005_5000); // PD1=5, PD2=5
-    clear_bits(PLL_USB_BASE + 0x04, 0x0000_0011); // Clear the PD and VCO bits to enable the PLL
-    while read_reg(PLL_USB_BASE + 0x00) & 0x8000_0000 == 0 {
-        // Wait for the PLL to be ready
-    }
+    // Reset and configure PLL_SYS with refdiv = 1, fbdiv = 125, pd1 = 6, pd2 = 2
+    // This 125 MHz
+    init_pll(PLL_SYS_BASE, RESET_PLL_SYS, 1, 125, 6, 2);
 
-    // Configure the CLK_SYS_CTRL register to configure the muxes and ultimately set CLK_SYS to the PLL.  (2.15.3.2)
-    // Set the aux mux to PLL_SYS (0 written to bits 5-7).  This is only safe to do right now because we set the glitchless mux to 0 above.
-    write_reg(
-        CLOCKS_BASE + 0x3c,
-        read_reg(CLOCKS_BASE + 0x3c) & !(0b111 << 5),
-    );
+    // Reset and configure PLL_USB with refdiv = 1, fbdiv = 100, pd1 = 5, pd2 = 5
+    // This is 48 Mhz
+    init_pll(PLL_USB_BASE, RESET_PLL_USB, 1, 100, 5, 5);
+
+    // Move CLK_REF over to the crystal oscillator
+    write_reg(CLOCKS_BASE + 0x34, 1 << 8); // Divider is 1
+    write_reg(CLOCKS_BASE + 0x30, 0x2); // SRC = XOSC (glitchless)
+
+    // Move CLK_SYS over to the aux src (which is the PLL)
+    write_reg(CLOCKS_BASE + 0x40, 1 << 8); // Divider is 1
+    clear_bits(CLOCKS_BASE + 0x3c, 0b111 << 5); // Auxsrc: PLL_SYS
     set_bits(CLOCKS_BASE + 0x3c, 0x0000_0001); // Set the glitchless mux to 1 (CLKSRC_CLK_SYS_AUX) so that we now use the PLL coming in on AUX.
+
+    // Set the USB clock to be the same as PLL_USB (which should be 48Mhz)
+    write_reg(CLOCKS_BASE + 0x54, 0); // Disable the clock by clearing bit 11.  This also sets AUXSRC to 0, which is PLL_USB
+    let _ = read_reg(CLOCKS_BASE); // Read the register just to stall for some cycles (we're waiting to make sure the clock peripheral clock is actually stopped)
+    set_bits(CLOCKS_BASE + 0x54, 1 << 11); // Enable it
+
+    // Set the ADC clock to be the same as PLL_USB (which should be 48Mhz)
+    write_reg(CLOCKS_BASE + 0x60, 0); // Disable the clock by clearing bit 11.  This also sets AUXSRC to 0, which is PLL_USB
+    let _ = read_reg(CLOCKS_BASE); // Read the register just to stall for some cycles (we're waiting to make sure the clock peripheral clock is actually stopped)
+    set_bits(CLOCKS_BASE + 0x60, 1 << 11); // Enable it
 
     // Set the peripheral clock to be the same as clk_sys
     write_reg(CLOCKS_BASE + 0x48, 0); // Disable the clock by clearing bit 11.  This also sets AUXSRC to 0, which is CLK_SYS
     let _ = read_reg(CLOCKS_BASE); // Read the register just to stall for some cycles (we're waiting to make sure the clock peripheral clock is actually stopped)
-    write_reg(CLOCKS_BASE + 0x48, 1 << 11); // Enable it
+    set_bits(CLOCKS_BASE + 0x48, 1 << 11); // Enable it
 
     // Configure the watchdog tick counter so that it divides by 12, leading to one tick every us.  (Because the XOSC is 12MHz.)
     // Without this being set properly, the TIMER doesn't count at the correct interval.
     write_reg(WATCHDOG_BASE + 0x2c, 12 | 1 << 9); // Set the divider to 12 and enable the watchdog
 
-    // Configure the timer not to pause during debugging. Otherwise, we can't single step through a delay()
+    // Configure the timer not to pause during debugging. Otherwise, we can't single step debug through a delay()
     // See...
     // https://github.com/raspberrypi/debugprobe/issues/45
     // https://github.com/raspberrypi/pico-sdk/issues/1586
     write_reg(TIMER_BASE + 0x2c, 0);
+}
+
+fn init_pll(pll_base: u32, peri_num: u32, refdiv: u32, fbdiv: u32, pd1: u32, pd2: u32) {
+    // Reset, then deassert the reset on the PLL
+    // See Section 2.14 in the datasheet for details
+    set_bits(RESETS_BASE, 1 << peri_num); // Write 1 to reset
+    clear_bits(RESETS_BASE, 1 << peri_num); // Write 0 to deassert reset
+
+    // Load refdiv and fbdiv
+    write_reg(pll_base + 0x00, refdiv);
+    write_reg(pll_base + 0x08, fbdiv);
+
+    // Clear the PD and VCO bits to enable the PLL
+    clear_bits(pll_base + 0x04, 1 << 0 | 1 << 5);
+    while read_reg(pll_base + 0x00) & 1 << 31 == 0 {
+        // Wait for the PLL to be ready
+    }
+
+    // Setup the post dividers
+    write_reg(pll_base + 0x0C, pd1 << 16 | pd2 << 12);
+
+    // Turn on the post divider
+    clear_bits(pll_base + 0x04, 1 << 3);
 }
